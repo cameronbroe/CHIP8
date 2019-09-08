e333beb66d06fa13502103a1177b80c3b4fbc7f6
Cameron Roe <croe@archermalmo.com>
2019-09-08 16:48:08 -0500

    Key input code done!

--- src/main.c
+++ src/main.c
@@ -24,6 +24,42 @@ int main(int argc, char** argv) {
     SetTargetFPS(60); // 60 frames per second! Helps out with delay and sound timers
     while(!WindowShouldClose()) {
         process_cycle();
+
+        // Keymaps
+        if(IsKeyDown(KEY_ONE)) { cpu.keypad[0x1] = 1; }
+        if(IsKeyDown(KEY_TWO)) { cpu.keypad[0x2] = 1; }
+        if(IsKeyDown(KEY_THREE)) { cpu.keypad[0x3] = 1; }
+        if(IsKeyDown(KEY_FOUR)) { cpu.keypad[0xC] = 1; }
+        if(IsKeyDown(KEY_Q)) { cpu.keypad[0x4] = 1; }
+        if(IsKeyDown(KEY_W)) { cpu.keypad[0x5] = 1; }
+        if(IsKeyDown(KEY_E)) { cpu.keypad[0x6] = 1; }
+        if(IsKeyDown(KEY_R)) { cpu.keypad[0xD] = 1; }
+        if(IsKeyDown(KEY_A)) { cpu.keypad[0x7] = 1; }
+        if(IsKeyDown(KEY_S)) { cpu.keypad[0x8] = 1; }
+        if(IsKeyDown(KEY_D)) { cpu.keypad[0x9] = 1; }
+        if(IsKeyDown(KEY_F)) { cpu.keypad[0xE] = 1; }
+        if(IsKeyDown(KEY_Z)) { cpu.keypad[0xA] = 1; }
+        if(IsKeyDown(KEY_X)) { cpu.keypad[0x0] = 1; }
+        if(IsKeyDown(KEY_C)) { cpu.keypad[0xB] = 1; }
+        if(IsKeyDown(KEY_V)) { cpu.keypad[0xF] = 1; }
+
+        if(IsKeyUp(KEY_ONE)) { cpu.keypad[0x1] = 0; }
+        if(IsKeyUp(KEY_TWO)) { cpu.keypad[0x2] = 0; }
+        if(IsKeyUp(KEY_THREE)) { cpu.keypad[0x3] = 0; }
+        if(IsKeyUp(KEY_FOUR)) { cpu.keypad[0xC] = 0; }
+        if(IsKeyUp(KEY_Q)) { cpu.keypad[0x4] = 0; }
+        if(IsKeyUp(KEY_W)) { cpu.keypad[0x5] = 0; }
+        if(IsKeyUp(KEY_E)) { cpu.keypad[0x6] = 0; }
+        if(IsKeyUp(KEY_R)) { cpu.keypad[0xD] = 0; }
+        if(IsKeyUp(KEY_A)) { cpu.keypad[0x7] = 0; }
+        if(IsKeyUp(KEY_S)) { cpu.keypad[0x8] = 0; }
+        if(IsKeyUp(KEY_D)) { cpu.keypad[0x9] = 0; }
+        if(IsKeyUp(KEY_F)) { cpu.keypad[0xE] = 0; }
+        if(IsKeyUp(KEY_Z)) { cpu.keypad[0xA] = 0; }
+        if(IsKeyUp(KEY_X)) { cpu.keypad[0x0] = 0; }
+        if(IsKeyUp(KEY_C)) { cpu.keypad[0xB] = 0; }
+        if(IsKeyUp(KEY_V)) { cpu.keypad[0xF] = 0; }
+
         BeginDrawing();
             ClearBackground(BLACK);
             for(int y = 0; y < VIDEO_HEIGHT; y++) {
@@ -41,6 +77,7 @@ int main(int argc, char** argv) {
                     }
                 }
             }
+            DrawFPS(0, 0);
         EndDrawing();
     }
     close_logger();
